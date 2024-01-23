bffcdabef997a0ff742f49afb98a46fc6a729ad4
Khang Nguyen <ngkhang0220@gmail.com>
2024-01-23 17:10:27 +0700

    M02 - Assg: Samer - Completed layout

--- 01_Assignments/02_HTML5_CSS3/01_Samar/js/index.js
+++ 01_Assignments/02_HTML5_CSS3/01_Samar/js/index.js
@@ -9,7 +9,7 @@ $(".owl-carousel").owlCarousel({
       items: 1,
     },
     600: {
-      items: 3,
+      items: 2,
     },
     1000: {
       items: 3,
@@ -17,10 +17,35 @@ $(".owl-carousel").owlCarousel({
   },
 });
 
-// 
+// Setup Fancybox
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
   Thumbs: {
     type: "classic",
   },
 });
+
+// Setup Counter-Up
+$('.counter').countUp({
+  'time': 2000,
+  'delay': 10
+});
+
+// Setup Wow.js
+new WOW().init();
+
+// Handle Scroll to Top
+const handleScrollToTop = () => {
+  window.scrollTo(0, 0);
+}
+
+// Handle visible button scroll to Top
+window.addEventListener('scroll', () => {
+  const OFFSET = 300;
+  const btnScrollToTop = document.getElementById('btnScrollToTop');
+  let windowTop = window.scrollY || document.documentElement.scrollTop;
+
+  (windowTop > OFFSET)
+    ? btnScrollToTop.classList.add('btn-is-visible')
+    : btnScrollToTop.classList.remove('btn-is-visible');
+})
