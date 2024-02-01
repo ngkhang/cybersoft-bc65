2b10d135001dba652c13a00d82f33627f1d0b123
Khang Nguyen <ngkhang0220@gmail.com>
2024-02-01 18:33:25 +0700

    M04 - Assg:  02_DOM JS - Update

--- 01_Assignments/04_JS/BT_02_DOM/js/index.js
+++ 01_Assignments/04_JS/BT_02_DOM/js/index.js
@@ -1,11 +1,64 @@
-function handleExerce(exerciseId) {
-  let exerciseID = document.getElementById(exerciseId);
-  let listExercise = document.getElementsByClassName('exercise');
-  let listBtnExercise = document.getElementsByClassName('btn-exercise');
+// Reset all input
+function resetAll(resultId) {
+  const inputs = document.getElementsByTagName('input');
 
-  let id = exerciseId.slice(-1);
+  resultId.innerHTML = '👉 Kết quả...';
+  resultId.classList.remove('resultSuccess', 'resultError');
+  resultId.classList.add('resultPedding');
+
+  [...inputs].forEach((input) => {
+    input.value = '';
+  });
+}
+
+// Fake loading
+function fakeLoading(exrciseId) {
+  const spinnerId = document.getElementById('spinner');
+  spinnerId.classList.remove('d-none');
+  spinnerId.classList.add('d-flex');
+
+  let timerId = setTimeout(() => {
+    spinnerId.classList.remove('d-flex');
+    spinnerId.classList.add('d-none');
+    exrciseId.classList.remove('d-none')
+  }, 500);
+}
+
+// Handle Submit with Enter keyboard
+function handleEnter(event, exerciseId) {
+  if (event.keyCode === 13) {
+    switch (exerciseId) {
+      case 'exercise01':
+        getSalary();
+        break;
+      case 'exercise02':
+        avergeNumber();
+        break;
+      case 'exercise03':
+        convertMoney();
+        break;
+      case 'exercise04':
+        calRectangle();
+        break;
+      default:
+        sumTwoNumber();
+        break;
+    }
+  };
+}
+
+// Handle Change Exercise
+function handleChangeExercise(exerciseId) {
+  const ID = exerciseId.slice(-1);
+  const exerciseID = document.getElementById(exerciseId);
+  const listExercise = document.getElementsByClassName('exercise');
+  const listBtnExercise = document.getElementsByClassName('btn-exercise');
+  const resultId = document.getElementById(`result_exercise_0${ID}`);
+
+  resetAll(resultId);
+
   [...listBtnExercise].forEach((ele, index) => {
-    (index === id - 1)
+    (index === ID - 1)
       ? ele.classList.add('active')
       : ele.classList.remove('active');
   });
@@ -14,76 +67,114 @@ function handleExerce(exerciseId) {
     ele.classList.add('d-none');
   });
 
-
-  let spinnerId = document.getElementById('spinner');
-  spinnerId.classList.remove('d-none');
-  spinnerId.classList.add('d-flex');
-
-  let timerId = setTimeout(() => {
-    spinnerId.classList.remove('d-flex');
-    spinnerId.classList.add('d-none');
-    exerciseID.classList.remove('d-none')
-  }, 500);
+  fakeLoading(exerciseID);
 }
 
 // BT 01 - Tính tiền lương nhân viên
 function getSalary() {
-  let dayId = document.getElementById('day');
-  let moneyInDayId = document.getElementById('moneyInDay');
-  let resultID = document.getElementById('result_exercise_01');
+  const dayId = document.getElementById('day');
+  const moneyInDayId = document.getElementById('moneyInDay');
+  const resultID = document.getElementById('result_exercise_01');
 
-  let output = dayId.value * moneyInDayId.value;
+  resultID.classList.remove('resultPedding', 'resultError', 'resultSuccess');
 
-  resultID.innerHTML = `${output}`;
+  let output;
+
+  if (!dayId.value * 1 && !moneyInDayId.value * 1) output = 'Vui lòng nhập input';
+  else if (dayId.value * 1 === NaN || moneyInDayId.value === NaN) output = 'Input không phải là số';
+  else if (!dayId.value * 1) output = 'Số ngày làm việc không hợp lệ';
+  else if (!moneyInDayId.value * 1) output = 'Tiền lương ngày làm việc không hợp lệ';
+  else output = dayId.value * 1 * moneyInDayId.value * 1;
+
+  resultID.classList.add(`${(Number.isInteger(output)) ? 'resultSuccess' : 'resultError'}`);
+
+  resultID.innerHTML = `👉 ${output}`;
 }
-
 
 // BT 02 - Tính giá trị trung bình của 5 số
 function avergeNumber() {
-  let listNumber = document.getElementsByName('number_exercise02');
-  let resultID = document.getElementById('result_exercise_02');
-
   const COUNT = 5;
+  const listNumber = document.getElementsByName('number_exercise02');
+  const resultID = document.getElementById('result_exercise_02');
+
+  resultID.classList.remove('resultPedding', 'resultError', 'resultSuccess');
+
   let total = 0;
   listNumber.forEach((ele) => {
     total += ele.value * 1;
   });
 
   let aver = total / COUNT;
-  resultID.innerHTML = `${aver}`;
+  resultID.classList.add('resultSuccess');
+  resultID.innerHTML = `👉 ${aver}`;
 }
-
 
 // BT 03 - Quy đổi tiền tệ: USD -> VND
 function convertMoney(USD = 23_500) {
-  let moneyId = document.getElementById('money');
-  let resultID = document.getElementById('result_exercise_03');
+  const moneyId = document.getElementById('money');
+  const resultID = document.getElementById('result_exercise_03');
 
-  let monneyConvert = (moneyId.value) ? moneyId.value * USD : 0;
+  resultID.classList.remove('resultPedding', 'resultError', 'resultSuccess');
 
-  resultID.innerHTML = `${new Intl.NumberFormat('vn-VN').format(monneyConvert)}`;
+  let money = moneyId.value;
+  let output;
+  if (money === '') output = 'Vui lòng nhập số tiền'
+  else if (!Number.isInteger(money * 1) || money * 1 < 0) output = 'Số tiền không hợp lệ'
+  else output = money * 1 * USD;
+
+  if (Number.isInteger(output)) {
+    resultID.classList.add('resultSuccess');
+    resultID.innerHTML = `👉 ${new Intl.NumberFormat('vn-VN').format(output)}`;
+  }
+  else {
+    resultID.classList.add('resultError');
+    resultID.innerHTML = `👉 ${output}`;
+  }
 }
-
 
 // BT 04 - Tính chu vi và diện tích hình chữ nhật
 function calRectangle() {
-  let longId = document.getElementById('long');
-  let wideId = document.getElementById('wide');
-  let resultID = document.getElementById('result_exercise_04');
+  const longId = document.getElementById('long');
+  const wideId = document.getElementById('wide');
+  const resultID = document.getElementById('result_exercise_04');
 
-  let S = longId.value * 1 * wideId.value * 1;
-  let P = (longId.value * 1 + wideId.value * 1) * 2;
+  resultID.classList.remove('resultPedding', 'resultError', 'resultSuccess');
 
-  resultID.innerHTML = `Diện tích: ${S}; Chu vi: ${P}`;
+  let longValue = longId.value;
+  let wideValue = wideId.value;
+
+  let output;
+  let S, P;
+  if (longValue === '' && wideValue === '') output = 'Vui lòng nhập chiều dài và chiều rộng';
+  else if (longValue === '') output = 'Vui lòng nhập chiều dài';
+  else if (wideValue === '') output = 'Vui lòng nhập chiều rộng';
+  else if (!Number.isInteger(longValue * 1) || longValue * 1 < 0) output = 'Chiều dài không hợp lệ';
+  else if (!Number.isInteger(wideValue * 1) || wideValue * 1 < 0) output = 'Chiều rộng không hợp lệ';
+  else {
+    S = longId.value * 1 * wideId.value * 1;
+    P = (longId.value * 1 + wideId.value * 1) * 2;
+    output = `Diện tích: ${S}; Chu vi: ${P}`;
+  }
+
+  resultID.classList.add(`${(Number.isInteger(S)) ? 'resultSuccess' : 'resultError'}`);
+  resultID.innerHTML = `👉 ${output}`;
 }
-
 
 // BT 05 - Tính tổng 2 ký số
 function sumTwoNumber() {
-  let numberId = document.getElementById('numberTwoCharactor');
-  let resultID = document.getElementById('result_exercise_05');
+  const numberId = document.getElementById('numberTwoCharactor');
+  const resultID = document.getElementById('result_exercise_05');
 
-  let number = numberId.value * 1;
+  resultID.classList.remove('resultPedding', 'resultError', 'resultSuccess');
 
-  resultID.innerHTML = `Tổng: ${Math.trunc(((number < 0) ? -number : number) / 10) + number % 10}`;
+  let output;
+  let numberVal = numberId.value;
+  if (numberVal === '') output = 'Vui lòng không bỏ trống';
+  else if (!Number.isInteger(numberVal * 1)) output = 'Vui lòng nhập chữ số';
+  else if (numberVal * 1 < -99 || numberVal * 1 > 99) output = 'Vui lòng nhập số có 2 chữ số';
+  else output = Math.trunc(((numberVal < 0) ? -numberVal : numberVal) / 10) + numberVal % 10;
+
+  resultID.classList.add(`${(Number.isInteger(output)) ? 'resultSuccess' : 'resultError'}`);
+
+  resultID.innerHTML = `👉 ${output}`;
 }
