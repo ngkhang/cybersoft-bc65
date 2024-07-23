7278ca460e9dc4c24bd2aabe6a17abdfff9b0166
Khang Nguyen <ngkhang0220@gmail.com>
2024-07-23 21:49:46 +0700

    M12 - Practices: 02_Youtube_clone - Login, Forget password, send Mail

--- 12_ExpressJS/02_Practices/02_youtube_clone/node43_backend/src/controllers/auth.controller.js
+++ 12_ExpressJS/02_Practices/02_youtube_clone/node43_backend/src/controllers/auth.controller.js
@@ -2,6 +2,8 @@ import initModels from "../models/init-models.js";
 import sequelize from "../models/connect.js";
 import { responseData } from '../config/response.js';
 import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import { sendMail } from '../config/mail.js';
 
 const model = initModels(sequelize);
 
@@ -56,7 +58,86 @@ const login = async (req, res) => {
   }
 };
 
+const loginFacebook = async (req, res) => {
+  let { faceAppId, fullName, email } = req.body;
+
+  let checkUser = await model.users.findOne({
+    where: {
+      face_app_id: id
+    }
+  });
+
+  if (!checkUser) {
+    let newData = {
+      full_name: fullName,
+      email,
+      pass_word: '',
+      avatar: '',
+      face_app_id: faceAppId,
+      role: 'USER',
+      refresh_token: '',
+    }
+
+    await model.users.create(newData);
+  }
+
+  let token = '';
+  responseData(token, 'Đăng nhập thành công', 200, res);
+}
+
+const forgetCheckMail = async (req, res) => {
+  let { email } = req.body;
+
+  let checkMail = await model.users.findOne({
+    where: {
+      email,
+    }
+  });
+
+  if (checkMail) {
+    let randomCode = crypto.randomBytes(5).toString('hex');
+    let newCode = {
+      code: randomCode,
+      expired: new Date(new Date().getTime() + 10 * 60000)
+    }
+    await model.code.create(newCode);
+
+    sendMail(email, "Mã xác thực", randomCode);
+
+    responseData('', 'Đăng nhập thành công', 200, res);
+    return;
+  }
+
+  responseData('', 'Email không đúng', 403, res);
+}
+
+const forgetCheckCode = async (req, res) => {
+  let { code } = req.body;
+
+  let checkCode = await model.code.findOne({
+    where: {
+      code,
+    }
+  })
+
+  if (checkCode) {
+    // Xóa code
+    await model.code.destroy({
+      where: {
+        id: checkCode.dataValues.id
+      },
+    });
+
+    responseData('', 'Thành công', 200, res);
+    return;
+  }
+  responseData('', 'Code không đúng', 403, res);
+}
+
 export {
   signUp,
   login,
+  loginFacebook,
+  forgetCheckMail,
+  forgetCheckCode,
 }
